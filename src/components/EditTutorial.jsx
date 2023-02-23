d9b2aed0dcede62da746665a0b2f5be1032a8edc
Ilknur Tuncer <ilknurtuncer67@gmail.com>
2023-02-24 00:35:07 +0300

    modal added

--- src/components/EditTutorial.jsx
+++ src/components/EditTutorial.jsx
@@ -12,3 +12,15 @@ const EditTutorial = () => {
                 aria-label="Close"
               ></button>
             </div>
+            <div className="modal-body">
+            <p>Modal body text goes here.</p>
+          </div>
+          <div className="modal-footer">
+            <button
+              type="button"
+              className="btn btn-secondary"
+              data-bs-dismiss="modal"
+            >
+              Close
+            </button>
+            
