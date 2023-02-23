214e28b98db3e873d5da9092410bff905f70b3bf
Ilknur Tuncer <ilknurtuncer67@gmail.com>
2023-02-24 00:28:44 +0300

    bootstrap add

--- src/components/TutorialList.jsx
+++ src/components/TutorialList.jsx
@@ -36,58 +36,58 @@ const TutorialList = ({ tutorials, getTutorials }) => {
     getTutorials();
   };
 
-//   return (
-//     <div className="container mt-4">
-//       <table className="table table-striped">
-//         <thead>
-//           <tr>
-//             <th scope="col">#id</th>
-//             <th scope="col">Title</th>
-//             <th scope="col">Description</th>
-//             <th scope="col" className="text-center">
-//               Edit
-//             </th>
-//           </tr>
-//         </thead>
-//         <tbody>
-//           {tutorials?.map((item) => {
-//             const { id, title, description } = item;
-//             return (
-//               <tr key={id}>
-//                 <th>{id}</th>
-//                 <td>{title}</td>
-//                 <td>{description}</td>
-//                 <td className="text-center text-nowrap">
-//                   <FaEdit
-//                     size={20}
-//                     type="button"
-//                     className="me-2 text-warning"
-//                     data-bs-toggle="modal"
-//                     data-bs-target="#edit-tutor"
-//                     // onClick={() =>
-//                     //   editTutorial({
-//                     //     id: 502,
-//                     //     title: "dokanmayin",
-//                     //     description: "REACT",
-//                     //   })
-//                     // }
-//                   />
-//                   <AiFillDelete
-//                     size={22}
-//                     type="button"
-//                     className="text-danger "
-//                     onClick={() => deleteTutorial(id)}
-//                   />
-//                 </td>
-//               </tr>
-//             );
-//           })}
-//         </tbody>
-//       </table>
+  return (
+    <div className="container mt-4">
+      <table className="table table-striped">
+        <thead>
+          <tr>
+            <th scope="col">#id</th>
+            <th scope="col">Title</th>
+            <th scope="col">Description</th>
+            <th scope="col" className="text-center">
+              Edit
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          {tutorials?.map((item) => {
+            const { id, title, description } = item;
+            return (
+              <tr key={id}>
+                <th>{id}</th>
+                <td>{title}</td>
+                <td>{description}</td>
+                <td className="text-center text-nowrap">
+                  <FaEdit
+                    size={20}
+                    type="button"
+                    className="me-2 text-warning"
+                    data-bs-toggle="modal"
+                    data-bs-target="#edit-tutor"
+                    // onClick={() =>
+                    //   editTutorial({
+                    //     id: 502,
+                    //     title: "dokanmayin",
+                    //     description: "REACT",
+                    //   })
+                    // }
+                  />
+                  <AiFillDelete
+                    size={22}
+                    type="button"
+                    className="text-danger "
+                    onClick={() => deleteTutorial(id)}
+                  />
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
 
-//       <EditTutorial />
-//     </div>
-//   );
-// };
+      <EditTutorial />
+    </div>
+  );
+};
 
-// export default TutorialList;
+export default TutorialList;
